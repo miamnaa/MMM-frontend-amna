4e61dc4cb55dd70579688326e7461b3f2ebf12ef
miamnaa <amna.minhas@convergentbt.com>
2026-08-12 13:20:29 +0500

    auto: sync local changes

--- src/app/features/calibrate/calibrate.ts
+++ src/app/features/calibrate/calibrate.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MsalService } from '@azure/msal-angular';
 
-import { localSignOut } from '../../core/auth/local-sign-out';
 import { DatasetService } from '../../core/services/dataset.service';
 import { TunnelService } from '../../core/services/tunnel.service';
 import { backendErrorMessage } from '../../shared/utils/backend-error';
@@ -26,7 +24,6 @@ export class Calibrate implements OnInit {
   private readonly router = inject(Router);
   private readonly datasetService = inject(DatasetService);
   private readonly tunnelService = inject(TunnelService);
-  private readonly msalService = inject(MsalService);
 
   readonly projectId = signal('');
   readonly datasetId = signal('');
@@ -49,15 +46,6 @@ export class Calibrate implements OnInit {
 
   toggleInfo(): void {
     this.infoOpen.update((open) => !open);
-  }
-
-  back(): void {
-    this.router.navigate(['/models', this.projectId()]);
-  }
-
-  /** See local-sign-out.ts - same call used everywhere else in the tunnel. */
-  signOut(): void {
-    void localSignOut(this.msalService);
   }
 
   save(): void {
